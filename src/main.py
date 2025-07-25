5eaa2cfd805f32aca51f12c17500788b5d25826f
tinkernegeroi <Roman.peskoff@gmail.com>
2025-07-25 14:05:43 +0300

    add parsers for Carcade and Alfa

--- src/main.py
+++ src/main.py
@@ -1,18 +1,21 @@
 import asyncio
 
 from src.Handlers.AlfaHandler import AlfaHandler
+from src.Handlers.CarcadeHandler import CarcadeHandler
 from src.Handlers.GazpromHandler import GazpromHandler
+from src.Services.CarcadeService import CarcadeService
 from src.Services.GazpromService import GazpromService
 from src.Handlers.VTBHandler import VTBHandler
 from src.Services.VTBService import VTBService
 from src.Parsers.PlaywrightParser import PlaywrightParser
-from src.Services.TgService import TgService
+from src.Services.AlfaService import AlfaService
 
 
 async def main():
     vtb_base_url = "https://www.vtb-leasing.ru/auto-market/f/type-is-4/?PAGEN_1"
     gazprom_baze_url = "https://autogpbl.ru/avtomobili-i-tekhnika-s-probegom/?filter-type=4"
     alfa_url = "https://t.me/s/alfaleasing_probeg"
+    carcade_url = "https://t.me/s/CarcadeStock"
 
     # async with PlaywrightParser(vtb_base_url) as parser:
     #     handler = VTBHandler()
@@ -27,11 +30,17 @@ async def main():
     #     data = await service.parse_n_pages(gazprom_baze_url, 5)
     #     for car in data:
     #         print(car)
+    #
+    # async with PlaywrightParser(alfa_url) as parser:
+    #     handler = AlfaHandler()
+    #     tg_parser = AlfaService(parser, handler, alfa_url)
+    #     await tg_parser.parse_page()
 
-    async with PlaywrightParser(alfa_url) as parser:
-        handler = AlfaHandler()
-        tg_parser = TgService(parser, handler)
-        await tg_parser.parse_page(alfa_url)
+    async with PlaywrightParser(carcade_url) as parser:
+        handler = CarcadeHandler()
+        tg_parser = CarcadeService(parser, handler, carcade_url)
+        await tg_parser.parse_page(carcade_url)
+
 
 
 if __name__ == "__main__":
