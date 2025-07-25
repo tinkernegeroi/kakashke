2ce0b2b1a68cc19119374f6bbdaecf2008cc75a6
tinkernegeroi <roman.peskoff@gmail.com>
2025-07-25 14:40:08 +0300

    update CarcadeHandler.py and CarcadeService.py

--- src/Handlers/CarcadeHandler.py
+++ src/Handlers/CarcadeHandler.py
@@ -7,9 +7,23 @@ class CarcadeHandler:
         return text.replace(" ", "").replace("\xa0", "").replace("км", "").replace("руб.", "")
 
     def get_title_from_text(self, text):
-        title = text[0][9:]
+        title = text[0][10:]
         return title
 
     def get_price_from_text(self, text):
-        price = text[2][4:]
+        price = text[2][5:]
         return self.replace_all(price)
+
+    def get_hashtags_from_text(self, text):
+        return text[-1]
+
+    def get_year_from_text(self, text):
+        return text[3][13:]
+
+    def get_city_from_text(self, text):
+        return text[4][16:]
+
+    def get_mileage_from_text(self, text):
+        mileage = text[5][7:]
+        mileage = self.replace_all(mileage)
+        return mileage
--- src/Services/CarcadeService.py
+++ src/Services/CarcadeService.py
@@ -9,10 +9,20 @@ class CarcadeService:
         for item in items:
             message_text = await self.parser.get_item_text(item, ".tgme_widget_message_text")
             text = self.handler.create_data_for_db(message_text)
-            title = self.handler.get_title_from_text(text)
-            price = self.handler.get_price_from_text(text)
-            result = {
-                "title": title,
-                "price": price
-            }
-            print(result)
+            hashtags = self.handler.get_hashtags_from_text(text)
+            if "#Легковые_CARCADE" in hashtags:
+                title = self.handler.get_title_from_text(text)
+                price = self.handler.get_price_from_text(text)
+                year = self.handler.get_year_from_text(text)
+                city = self.handler.get_city_from_text(text)
+                mileage = self.handler.get_mileage_from_text(text)
+                result = {
+                    "title": title,
+                    "price": price,
+                    "year": year,
+                    "city": city,
+                    "mileage": mileage
+                }
+                print(result)
+            else:
+                continue
