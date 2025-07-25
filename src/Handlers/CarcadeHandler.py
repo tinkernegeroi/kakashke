6a5c5878904bb3a18c1de0a6092a7a354e7a0d9d
tinkernegeroi <Roman.peskoff@gmail.com>
2025-07-25 14:04:48 +0300

    update CarcadeHandler.py and CarcadeService.py

--- src/Handlers/CarcadeHandler.py
+++ src/Handlers/CarcadeHandler.py
@@ -1,3 +1,15 @@
 class CarcadeHandler:
+    def create_data_for_db(self, text):
+        text_s = text.split("\n")
+        return text_s
+
     def replace_all(self, text):
-        return text.replace(" ", "").replace("\xa0", "").replace("км", "")
+        return text.replace(" ", "").replace("\xa0", "").replace("км", "").replace("руб.", "")
+
+    def get_title_from_text(self, text):
+        title = text[0][9:]
+        return title
+
+    def get_price_from_text(self, text):
+        price = text[2][4:]
+        return self.replace_all(price)
--- src/Services/CarcadeService.py
+++ src/Services/CarcadeService.py
@@ -8,4 +8,11 @@ class CarcadeService:
         items = await self.parser.get_items_from_tg(url)
         for item in items:
             message_text = await self.parser.get_item_text(item, ".tgme_widget_message_text")
-            print(message_text)
+            text = self.handler.create_data_for_db(message_text)
+            title = self.handler.get_title_from_text(text)
+            price = self.handler.get_price_from_text(text)
+            result = {
+                "title": title,
+                "price": price
+            }
+            print(result)
